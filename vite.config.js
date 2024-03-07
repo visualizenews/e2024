75187fe314d3172a470042a4321269c95fe40c37
carlo <littleark@gmail.com>
2024-03-07 14:33:34 +0100

    reverted base to none

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
     outDir: "./dist", // relative to index.html
     // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
   },
-  base: "/e2024/",
+  //base: "/e2024/",
   // base: "./",
 });
