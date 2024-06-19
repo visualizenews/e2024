885717f0449996b6c4f7b9d8f30a7f1f13f18e08
carlo <littleark@gmail.com>
2024-06-19 14:33:11 +0200

    forgotten Social file

---
+++ src/lib/Social.svelte
@@ -0,0 +1,82 @@
+<script>
+    let url = "https://2024.visualize.news";
+</script>
+
+<ul>
+    <li>
+        <a
+            href="https://x.com/intent/post?text=Exploring the elections in 2024 with charts and analysis+by+%40visualizenews+and+%40littleark+{encodeURIComponent(
+                url,
+            )}&hashtags=dataviz,datavisualization,elections"
+            target="_blank"
+            aria-label="Share on twitter"
+            title="Share on twitter"
+            ><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                ><path
+                    d="M13.35 10.93l5.53-6.43h-1.31l-4.8 5.59L8.92 4.5H4.5l5.8 8.45-5.8 6.74h1.31l5.08-5.9 4.05 5.9h4.42l-6.01-8.76zm-1.8 2.09l-.59-.84-4.68-6.7H8.3l3.77 5.4.6.85 4.9 7.02h-2.01l-4.01-5.73z"
+                    fill="#242424"
+                ></path></svg
+            ></a
+        >
+    </li>
+    <li>
+        <a
+            href="https://www.facebook.com/sharer/sharer.php?u={encodeURIComponent(
+                url,
+            )}&t={encodeURIComponent('2024 Elections Year')}"
+            target="_blank"
+            aria-label="Share on facebook"
+            title="Share on facebook"
+            ><svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                class="fh adg"
+                ><path
+                    d="M22 12.06C22 6.5 17.52 2 12 2S2 6.5 2 12.06c0 5.02 3.66 9.19 8.44 9.94v-7.03H7.9v-2.9h2.54V9.83c0-2.52 1.5-3.91 3.77-3.91 1.1 0 2.24.2 2.24.2V8.6H15.2c-1.24 0-1.63.78-1.63 1.57v1.9h2.78l-.45 2.9h-2.33V22A10.04 10.04 0 0 0 22 12.06z"
+                    fill="currentColor"
+                ></path></svg
+            ></a
+        >
+    </li>
+    <li>
+        <a
+            href="https://www.linkedin.com/sharing/share-offsite/?url={encodeURIComponent(
+                url,
+            )}"
+            target="_blank"
+            aria-label="Share on linkedin"
+            title="Share on linkedin"
+            ><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                ><path
+                    d="M21 4.32v15.36A1.32 1.32 0 0 1 19.68 21H4.32A1.32 1.32 0 0 1 3 19.68V4.32A1.32 1.32 0 0 1 4.32 3h15.36A1.32 1.32 0 0 1 21 4.32zM8.3 9.9H5.65v8.47h2.63V9.9h.02zm.22-2.92A1.52 1.52 0 0 0 7 5.44h-.05a1.53 1.53 0 0 0 0 3.06 1.52 1.52 0 0 0 1.56-1.48v-.05zm9.82 6.24c0-2.56-1.63-3.54-3.23-3.54a3.02 3.02 0 0 0-2.67 1.37h-.08V9.87H9.87v8.48h2.65v-4.5a1.75 1.75 0 0 1 1.58-1.88h.1c.84 0 1.47.52 1.47 1.85v4.54h2.65l.02-5.14v-.01z"
+                    fill="currentColor"
+                ></path></svg
+            ></a
+        >
+    </li>
+</ul>
+
+<style>
+    ul {
+        display: flex;
+        list-style: none;
+        padding: 0;
+        justify-content: center;
+        align-items: center;
+    }
+
+    li {
+        margin: 0;
+    }
+
+    a {
+        color: #242424;
+        display: inline-block;
+        width: 50px;
+        text-align: center;
+        vertical-align: middle;
+        padding: 10px;
+    }
+</style>
